d2b2c6daac37db94268bd779df409d64007c378a
Reid Jackson <72239107+rlj0713@users.noreply.github.com>
2026-02-15 20:18:06 +0000

    Changed phase 2 to reference game_data

--- example/phases_examples/phase_2.py
+++ example/phases_examples/phase_2.py
@@ -7,48 +7,54 @@
 # To make this work, you may have to type this into the terminal --> pip install curses
 import curses
 
-# Icons
-# To find more icons go to https://emojipedia.org/
-turtle = "\U0001F422"
-eagle = "\U0001F985"
-obstacle = "\U0001FAA8 "
-leaf = "\U0001F343"
-empty = "  "  # two spaces for alignment
+game_data = {
+    # Icons
+    # To find more icons go to https://emojipedia.org/
+    "turtle": "\U0001F422",
+    "eagle": "\U0001F985",
+    "obstacle": "\U0001FAA8 ",
+    "leaf": "\U0001F343",
+    "empty": "  ",
 
-# Board dimensions
-width, height = 5, 5
+    # Board dimensions
+    'width': 5,
+    'height': 5,
 
-# Icon positions
-positions = {
-    'player': (0, 0),
-    'eagle': (4, 4),
-    'obstacle': [(1, 2), (3, 1)],
-    'leaf': [(2, 1)],
+    # Icon positions
+    'positions': {
+        'player': (0, 0),
+        'eagle': (4, 4),
+        'obstacle': [(1, 2), (3, 1)],
+        'leaf': [(2, 1)],
+    }
 }
 
 def draw_board(stdscr):
-    # Initialize color support
     curses.start_color()
     curses.use_default_colors()
     curses.init_pair(1, curses.COLOR_WHITE, -1)
+
+    width = game_data['width']
+    height = game_data['height']
+    positions = game_data['positions']
 
     stdscr.clear()
     for y in range(height):
         row = ""
         for x in range(width):
             if (x, y) == positions['player']:
-                row += turtle
+                row += game_data['turtle']
             elif (x, y) == positions['eagle']:
-                row += eagle
+                row += game_data['eagle']
             elif (x, y) in positions['obstacle']:
-                row += obstacle
+                row += game_data['obstacle']
             elif (x, y) in positions['leaf']:
-                row += leaf
+                row += game_data['leaf']
             else:
-                row += empty
+                row += game_data['empty']
         stdscr.addstr(y, 0, row, curses.color_pair(1))
 
     stdscr.refresh()
-    stdscr.getkey()
+    stdscr.getkey()  # pause to see board
 
 curses.wrapper(draw_board)
