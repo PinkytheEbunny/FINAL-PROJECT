664d0ed29978f21ba33e8fc3f7451d5f6945f930
PinkytheEbunny <JTaylor080910@gmail.com>
2026-02-20 15:38:49 +0000

    Start menu

--- student_work/game.py
+++ student_work/game.py
@@ -1,11 +1,15 @@
 # Write your game here
+def start_menu()
+    print("               ")
+    
+    print("    WELCOME    ")
+    print(" TO DOODLE JUMP")
+# game_data = {
+#     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
+# }
 
-game_data = {
-    # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
-}
-
-def draw_board(screen):
+# def draw_board(screen):
     # Print the board and all game elements using curses
 
-
+start_menu()
 # Good Luck!
