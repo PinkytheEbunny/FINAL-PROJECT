34f201b7499dceb7b5b85150415232fa77e48bc6
PinkytheEbunny <JTaylor080910@gmail.com>
2026-03-04 15:50:55 +0000

    Added gravity

--- student_work/game.py
+++ student_work/game.py
@@ -7,37 +7,47 @@
 import curses
 import random
 collectable_x = random.randint(1,9)
-collectable_y = random.randint(7,10)
+collectable_y = random.randint(8,10)
 
 collectable_xx = random.randint(1,9)
-collectable_yy = random.randint(2,5)
+collectable_yy = random.randint(5,6)
+
+collectable_xxx = 5
+collectable_yyy = 13
+cieling = 2
 game_data = {
     'width': 10,
     'height': 15,
     'player': {"x": 5, "y": 13, "score": 0, "energy": 10, "max_energy": 10},
+    'floor': [
+        {"x": 1, "y": 14, 'state': 'solid'},
+        {"x": 2, "y": 14, 'state': 'solid'},
+        {"x": 3, "y": 14, 'state': 'solid'},
+        {"x": 4, "y": 14, 'state': 'solid'},
+        {"x": 5, "y": 14, 'state': 'solid'},
+        {"x": 6, "y": 14, 'state': 'solid'},
+        {"x": 7, "y": 14, 'state': 'solid'},
+        {"x": 8, "y": 14, 'state': 'solid'},
+        {"x": 9, "y": 14, 'state': 'solid'},
+        {"x": 10, "y": 14, 'state': 'solid'}
+    ],
     'collectibles': [
         {"x": collectable_x, "y": collectable_y, "collected": False},
         {"x": collectable_xx, "y": collectable_yy, "collected": False},
+        {"x": collectable_xxx, "y": collectable_yyy, "collected": False},
     ],
     'obstacles': [
 
         {"x": collectable_x, "y": collectable_y+1},
         {"x": collectable_xx, "y": collectable_yy+1},
-        {"x": 1, "y": 14},
-        {"x": 2, "y": 14},
-        {"x": 3, "y": 14},
-        {"x": 4, "y": 14},
-        {"x": 5, "y": 14},
-        {"x": 6, "y": 14},
-        {"x": 7, "y": 14},
-        {"x": 8, "y": 14},
-        {"x": 9, "y": 14},
-        {"x": 10, "y": 14},
     ],
 
     # ASCII icons
     'turtle': "\U0001F419",
+    'state': "\U0001F533",
+    'solid': "\U0001F533",
     'obstacle': "\U0001F533",
+    'Lava': "\U0001F7E7",
     'leaf': "\U0001FA99",
     'empty': "  "
 }
@@ -77,13 +87,16 @@ def move_player(key):
     key = key.lower()
 
     if key == "w" and y > 0:
-        new_y -= 1
+        if (game_data['player']['x']) == collectable_x and (game_data['player'])['y'] == collectable_y or (game_data['player']['x']) == collectable_xx and (game_data['player'])['y'] == collectable_yy or (game_data['player']['x']) == collectable_xxx and (game_data['player'])['y'] == collectable_yyy:
+            new_y -= 6
     elif key == "s" and y < game_data['height'] - 1:
         new_y += 1
     elif key == "a" and x > 0:
         new_x -= 1
+        new_y += 1
     elif key == "d" and x < game_data['width'] - 1:
         new_x += 1
+        new_y += 1
     else:
         return  # Invalid key or move off board
 
@@ -96,7 +109,13 @@ def move_player(key):
     game_data['player']['y'] = new_y
     if (game_data['player']['x']) == collectable_x and (game_data['player'])['y'] == collectable_y:
         (game_data['player']['score']) += 1
-        
+    if (game_data['player']['x']) == collectable_xx and (game_data['player'])['y'] == collectable_yy:
+        (game_data['player']['score']) += 1
+    if (game_data['player']['y']) <= cieling:
+        (game_data['player']['y']) = 13
+        (game_data['obstacles']['x']) = random.randint(1,9)
+        (game_data['obstacles']['y']) = random.randint(1,9)
+
 
 
 def main(stdscr):
